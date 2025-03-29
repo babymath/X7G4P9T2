77fa11ea772d6f58beb96591041d0e4ecd396316
babymath <164469039+babymath@users.noreply.github.com>
2025-03-29 16:43:30 +0530

    Create quizzes.js

---
+++ change/quizzes.js
@@ -0,0 +1,12 @@
+const quizzes = [
+    { 
+        title: "Bihar Historical Place", 
+        file: "bihar historical place.js", 
+        subject: "History"
+    },
+    { 
+        title: "Time and Work/ समय और कार्य", 
+        file: "time-work-1.js", 
+        subject: "Mathematics"  
+    }
+];
