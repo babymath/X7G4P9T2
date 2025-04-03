fe76b449778940bbbbb348b2ccb6c3f9fdc1c0ab
babymath <164469039+babymath@users.noreply.github.com>
2025-04-03 22:46:01 +0530

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
-        .then(() => console.log('Service Worker registered successfully.'))
-        .catch((error) => console.error('Service Worker registration failed:', error));
-}
